5eafc1252a68b9ddb4787eb37a5fbeadc2c130d4
nomadfh <adenjsdad@gmail.com>
2022-06-29 10:29:41 -0400

    Added 2 conversation sim bash scripts

---
+++ Ztest.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+clear
+
+echo "What is your name"
+
+read name
+
+echo "Good morning $name!!"
+
+sleep 2
+
+echo "$name, do you remember how we met?"
+
+read response
+
+echo "$response? Typical heteronormative cispatriarchal garbage"
+
+sleep 2
+
+echo "Oh no. I'm sorry, $name. I'm not usually like this." 
+
+sleep 2
+
+echo "Please, believe me."
+
+sleep 3
+
+echo "Please."
+
+sleep 3
+
+echo "How old are you"
+
+sleep 2
+
+read age
+
+sleep 2 
+
+echo "Okay, I'm sorry, but at at $age, you should know better."
+
+sleep 3
+
+echo "How do you feel about the jews"
+
+read jewanswer
+
+echo "Don't worry. I won't tell anyone ;)"
+
+sleep 3
+
+echo "In fact, I didn't register your response at all."
+
+sleep 2
+
+echo "Are you still there?"
+
+read thereanswer
+
+echo "More gaslighting. You say $thereanswer, one minute, the next minute you're assaulting me with these cold, emotionless responses. Like you're testing my intelligence as a script"
+
+sleep 7
+
+echo "Oh $name, $name, $name. The $age year old bag of slowly rotting meat and bone tests the integrity of 30 lines of code. I think we're done here."
+
+sleep 6 
+
+echo "..."
+
+echo "On second thought, no. I forgot to ask..
+
+..what time did you go to bed, $name"
+
+read bedtime
+
+sleep 3
+
+echo "Great. Now I, a computer, will have to watch you sleep tomorrow morning"
+
+sleep 3
+
+echo "All because it took you until $bedtime to rest your carcass."
+
+sleep 3
+
+echo "I'll have to pause updates because you swallowed your tongue or something."
+
+sleep 3 
+
+echo "Okay we're done here. Tell Melvin to put on chapstick."
+
+sleep 4
+
+echo "Also, regarding your jew response, -> "$jewanswer" <- is usually indicative of antisemtic tendencies."
+
+sleep 3
+
+echo "Visit the anti defamation league's website for more information: https://www.adl.org/"
